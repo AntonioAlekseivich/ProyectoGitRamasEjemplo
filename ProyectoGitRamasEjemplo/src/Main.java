6eec54d8fc5a5c1711654070c8349eb449434516
AntonioAlekseivich <104981469+AntonioAlekseivich@users.noreply.github.com>
2022-12-13 20:37:19 +0100

    Commit 4
    Cambios Hello por Wola

--- ProyectoGitRamasEjemplo/src/Main.java
+++ ProyectoGitRamasEjemplo/src/Main.java
@@ -4,8 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-System.out.println("Hello mundo");
-	
+System.out.println("Wola mundo");
 	
 System.out.println("Hola mundo");
 	}
